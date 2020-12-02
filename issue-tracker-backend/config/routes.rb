aec1bf8a8eb5f9bf20935cca0e603dc887f41add
no0ro <nrolwes@gmail.com>
2020-12-02 08:01:12 -0500

    nested issues inside projects routes

--- issue-tracker-backend/config/routes.rb
+++ issue-tracker-backend/config/routes.rb
@@ -1,17 +1,22 @@
 Rails.application.routes.draw do
+# 1. add namespacing so its clear we're fetching to api and version 1
+# 2. set up nested routes for projects
+      # ex:  api/v1/projects/1/issues
+      # prevented : api/v1/issues -- aka see allll issues for every proj in the db
 
-# add namespacing so its clear we're fetching to api and version 1
-  namespace :api do  # add in api before every route
-    namespace :v1 do
-      resources :issues
-      resources :projects
+  namespace :api do  # added in api before every route
+    namespace :v1 do 
+      # resources :issues  ---- # adding this back in would allow us to access api/v1/issues again
+      resources :projects do
+        resources :issues
+      end
     end 
   end
+
 end
 
-
-# fetch('http://localhost:3000/api/v1/issues')
+# Notes: 
+# fetch('http://localhost:3000/api/v1/projects/1/issues')
 # rails s -p 4000 (the way to change ports)
 
-# the routes we set up in ouir rails app, are the routes we'll be making fetch
-# requests to in our frontend 
+# the routes we set up in ouir rails app, are the routes we'll be making fetch requests to in our frontend 
