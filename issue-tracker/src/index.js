b0f60b927267f133d24af80ccded3ef3f4b2c3e9
no0ro <nrolwes@gmail.com>
2020-12-03 08:51:15 -0500

    instantiated createStore fn with a reducer and thunk middleware for initial state. set compose fn equal to enhancer variable

--- issue-tracker/src/index.js
+++ issue-tracker/src/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {createStore, applyMiddleware}from 'redux';
+import {createStore, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk'
-import {Provider} from 'react-redux'
+import {Provider} from 'react-redux' // any component we wrap with {Provider}, gets access to Redux Store
 
+import App from './App';  
 
-import App from './App';
+let enhancer = compose()
+// middleware can be async so should be 1st store enhancer in composition chaain 
+
+// set up store 
+let store = createStore(reducer, applyMiddleware(thunk))
+// createStore takes AT LEAST 2 args([reducers], [initialState])
+
 
 ReactDOM.render(
   // <React.StrictMode>
