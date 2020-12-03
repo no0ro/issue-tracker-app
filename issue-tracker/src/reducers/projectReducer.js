95dac908537fd2abb20bf3fada591a4ec4cc2c3a
no0ro <nrolwes@gmail.com>
2020-12-03 10:22:07 -0500

    added reducer fn  with params state set equal to object of arrays, and an obj action

---
+++ issue-tracker/src/reducers/projectReducer.js
@@ -0,0 +1,5 @@
+// just a function! responsible for updating parts of store that have to do with our Project
+
+export default function projectReducer(state = {accounts: []}, action){
+    return state
+}
