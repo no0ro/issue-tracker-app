9a41afb90d8bc6ba11f8b736d968e95df65fb65f
no0ro <nrolwes@gmail.com>
2020-12-15 09:22:32 -0500

    added basic class syntax to test

--- issue-tracker/src/components/ProjectInput.js
+++ issue-tracker/src/components/ProjectInput.js
@@ -1,1 +1,17 @@
-// form to make new Projects
+// form to make new Projects. bc form, needs state, so Class
+
+import React from 'react'
+
+class ProjectInput extends React.Component{
+
+    render() {
+        return (
+            <div>   
+                inside ProjectInput.js!
+            </div>
+        )
+
+    }
+}
+
+export default ProjectInput
