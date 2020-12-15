074250ec40dd45b8cf5d4fb7d100ff2ef6eae3be
no0ro <nrolwes@gmail.com>
2020-12-15 12:07:49 -0500

    comments, passed projects props to the Projects component

--- issue-tracker/src/containers/ProjectsContainer.js
+++ issue-tracker/src/containers/ProjectsContainer.js
@@ -6,23 +6,27 @@ import Projects from '../components/Projects'
 import ProjectInput from '../components/ProjectInput'
 
 class ProjectsContainer extends React.Component {
- 
+   // 2ish. accessing props inside the ProjectsContainer component
+
     componentDidMount() {
         // fetchProjects() // issue is that this call inst connect to Redux Store // SO must access function through props // gave access to fetchProjects fn  when we imported 
+        // when this container mounts, we have to make this request to the backend
         this.props.fetchProjects() // and add {fetchProjects} to connect to give access
     }
-    
-    render () {
-        return (
 
+    render () {
+        // 3ish then sending those accounts down to our <Projects /> component
+        return (
+            
             <div>   
                 <ProjectInput/>
-                <Projects/> 
+                <Projects projects={this.props.projects}/> 
             </div>
         )
     }
 }
 
+// 1ish. get projects from our redux store
 const mapStateToProps = state => {
     // give this component, access through props, to the projects inside our state
     // turn data into props for fetchProjects to use
