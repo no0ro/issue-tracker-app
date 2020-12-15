0a7f3ad6d19b8dbc162fbc29009a3a9c539ed5da
no0ro <nrolwes@gmail.com>
2020-12-15 11:06:55 -0500

    working in Thunk async functionality, added dispatch fn

--- issue-tracker/src/actions/fetchProjects.js
+++ issue-tracker/src/actions/fetchProjects.js
@@ -1,7 +1,19 @@
 export function fetchProjects() {
-    
-    fetch('http://localhost:3000/api/v1/projects')
-    .then(res => res.json())
-    .then(data => console.log(data))
-    
+    // console.log('inside fetch projects')
+
+    // Thunk Closure! 
+    return (dispatch) => {
+    // dispatch as arg so can use it as action creator and dispatch the responce from our fetch frequests. Thanks Thunk
+        
+        fetch('http://localhost:3000/api/v1/projects')
+        .then(res => res.json())
+        .then(projectsData => dispatch({
+            // MUST have 'type' key
+            type: 'FETCH_PROJECTS',
+            payload: projectsData
+        })) // to update Redux Store, return action object
+        // dispatching to reducer with a type of FETCH_PROJECTS and value projData
+        // .then(data => console.log(data))
+
+    }
 }
