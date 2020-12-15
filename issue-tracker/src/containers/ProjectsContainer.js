1f23746a03b49d7fcfd9b8d3c3e4049182e24742
no0ro <nrolwes@gmail.com>
2020-12-15 10:02:30 -0500

    imported connect and declared mapStateToProps fn

--- issue-tracker/src/containers/ProjectsContainer.js
+++ issue-tracker/src/containers/ProjectsContainer.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { connect } from 'react-redux';
+
 import Projects from '../components/Projects'
 import ProjectInput from '../components/ProjectInput'
 
@@ -15,4 +17,13 @@ class ProjectsContainer extends React.Component {
     }
 }
 
-export default ProjectsContainer
+const mapStateToProps = state => {
+    // give this component, access through props, to the projects inside our state
+    return 
+        projects: projects.state
+
+}
+
+export default connect()(ProjectsContainer)
+
+// ask, do we want to map state to props? or dispatch to props?
