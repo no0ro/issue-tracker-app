6634bcf008d89c7a60ea0faa3c3b295eb467fe73
no0ro <nrolwes@gmail.com>
2020-12-15 10:44:32 -0500

    added return to mapStateToProps fn, passed args to connect function

--- issue-tracker/src/containers/ProjectsContainer.js
+++ issue-tracker/src/containers/ProjectsContainer.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { connect } from 'react-redux';
 
+import {fetchProjects} from '../actions/fetchProjects.js'
 import Projects from '../components/Projects'
 import ProjectInput from '../components/ProjectInput'
 
 class ProjectsContainer extends React.Component {
+
 
     render () {
         return (
@@ -19,11 +21,17 @@ class ProjectsContainer extends React.Component {
 
 const mapStateToProps = state => {
     // give this component, access through props, to the projects inside our state
-    return 
-        projects: projects.state
+    // turn data into props for fetchProjects to use
+
+    return {
+        projects: state.projects
+        // NOTE: return as {}
+    }
 
 }
 
-export default connect()(ProjectsContainer)
+export default connect(mapStateToProps, {fetchProjects})(ProjectsContainer)
+// connect(GET from store, pass actioncreators/reducers)(ProjectsContainer)
+// {fetchProjects} - equivelent to mapping dispatchToProps 
 
-// ask, do we want to map state to props? or dispatch to props?
+// ask, do we want to map state to props? or dispatch to props? or both?
