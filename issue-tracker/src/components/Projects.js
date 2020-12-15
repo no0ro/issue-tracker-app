e869a9a70da4071cbd213d82f417d65c5ec14e8e
no0ro <nrolwes@gmail.com>
2020-12-15 09:44:19 -0500

    added functional component Projects

--- issue-tracker/src/components/Projects.js
+++ issue-tracker/src/components/Projects.js
@@ -1,1 +1,12 @@
-// list of all our Projects
+// list of all our Projects - functional component
+import React from 'react'
+
+const Projects = (props) => {
+    return (
+        <div>   
+            Inside Projects.js!
+        </div>
+    )
+}
+
+export default Projects
