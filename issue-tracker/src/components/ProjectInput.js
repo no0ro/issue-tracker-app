a770c5d9bf115df773277b0ca975cab8b4a97273
no0ro <nrolwes@gmail.com>
2020-12-16 03:03:02 -0500

    added local initial state and set the input value - changed from uncontrolled form, to controlled

--- issue-tracker/src/components/ProjectInput.js
+++ issue-tracker/src/components/ProjectInput.js
@@ -3,15 +3,15 @@
 import React from 'react'
 
 class ProjectInput extends React.Component{
+    // SET STATE - giving form state and value make it a controlled form. 
+    state = { name: ''} // could use constructor syntax instead
 
     render() {
         return (
             <div>   
                 <form>
                     <label> Project Name: </label>
-                    <input type='text' placeholder="Name"/> 
-                    
-
+                    <input type='text' placeholder="Name" value={this.state.name}/> 
                 </form>
             </div>
         )
