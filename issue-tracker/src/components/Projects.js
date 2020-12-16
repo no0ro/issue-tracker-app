b188bb2243cc284513ee17177191b584139937b7
no0ro <nrolwes@gmail.com>
2020-12-16 06:56:06 -0500

    imported Project, render Project component instead of list data

--- issue-tracker/src/components/Projects.js
+++ issue-tracker/src/components/Projects.js
@@ -1,11 +1,12 @@
 // list of all our Projects - functional component
 import React from 'react'
+import Project from './Project'
 
 const Projects = (props) => {
     // console.log(props) // an obj of all the props passed in
     return (
         <div>   
-            {props.projects.map(project => <li key={project.id}> {project.name} </li>)}
+            {props.projects.map(project => <li key={project.id}> <Project/> </li>)}
         </div>
     )
 }
