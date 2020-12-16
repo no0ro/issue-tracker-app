be536ecdcd514e360770867fdbf55e54d0feb15c
no0ro <nrolwes@gmail.com>
2020-12-16 09:43:16 -0500

    imported ItemsContainer, reutrn component, and reformatted JSX tags

--- issue-tracker/src/components/Project.js
+++ issue-tracker/src/components/Project.js
@@ -1,22 +1,25 @@
 import React from 'react'
 import {Redirect} from 'react-router-dom'
 
+import ItemsContainer from '../containers/ItemsContainer'
+
 // functional bc just diaplaying. doesnt need to update or directly access state
 const Project = (props) => {
   // console.log(props)
   // props.projects is an array. its alllll of our projects
-  let project = props.projects[props.match.params.id - 1]
-    // bc index starts at 0
-
+  let project = props.projects[props.match.params.id - 1] // bc index starts at 0
   console.log(project)
 
     return (
         // going to need access to a Project, so pass down props
         // CANNOT RENDER AN OBJ inside jsx tags
-      <li>
-        {project ? null : <Redirect to='/projects'/>}
-        {project ? project.name : null}
-      </li>
+      <div>
+        <h2>
+          {project ? null : <Redirect to='/projects'/>}
+          {project ? project.name : null}
+        </h2>
+          <ItemsContainer/>
+      </div>
     )
 
 }
