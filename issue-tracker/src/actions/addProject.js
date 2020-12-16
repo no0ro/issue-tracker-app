0d661ef01d7ccdbd197948b923fd49f7a905948a
no0ro <nrolwes@gmail.com>
2020-12-16 04:21:23 -0500

    added in fetch to post

--- issue-tracker/src/actions/addProject.js
+++ issue-tracker/src/actions/addProject.js
@@ -1,5 +1,16 @@
-// passed in data from setState in ProjectInput from
+// passed in the setState formData from ProjectInput Form
 
 export const addProject = (formData) => {
+    // must pass in dispatch so we can import it into the input file and then connect it to the action
 
+    return (dispatch) => {
+        fetch('http://localhost:3000/api/v1/projects', {
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            method: 'POST',
+            body: JSON.stringify(formData)
+        })
+    }
 }
