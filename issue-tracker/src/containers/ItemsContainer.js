adca0dccc6814556d40ee8653a3e399851d1dedb
no0ro <nrolwes@gmail.com>
2020-12-16 09:54:26 -0500

    imported items and ittemsInput into items container and rendered components

--- issue-tracker/src/containers/ItemsContainer.js
+++ issue-tracker/src/containers/ItemsContainer.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import ItemInput from '../components/ItemInput'
+import Items from '../components/Items'
 
 
 class ItemsContainer extends React.Component {
-     
+     render () {
+         return (
+             <div>
+                 <ItemInput/>
+                 <Items/>
+             </div>
+         )
+     }
 
 }
 
