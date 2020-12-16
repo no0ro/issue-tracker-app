d3bc4e03804a50325a4c906edf04c75ed8196eef
no0ro <nrolwes@gmail.com>
2020-12-16 07:13:22 -0500

    display component as list

--- issue-tracker/src/components/Project.js
+++ issue-tracker/src/components/Project.js
@@ -2,12 +2,13 @@ import React from 'react'
 
 // functional bc just diaplaying. doesnt need to update or directly access state
 const Project = (props) => {
-
+  // console.log(props)
     return (
         // going to need access to a Project, so pass down props
-      <div>
-        hi
-      </div>
+        // CANNOT RENDER AN OBJ inside jsx tags
+      <li>
+        {props.project.name}
+      </li>
     )
 
 }
