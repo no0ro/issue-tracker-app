898d020788ff04a322a303ff6fef9ca0a6fb92c8
no0ro <nrolwes@gmail.com>
2020-12-16 10:54:28 -0500

    comment

--- issue-tracker/src/components/Project.js
+++ issue-tracker/src/components/Project.js
@@ -8,10 +8,9 @@ const Project = (props) => {
   // console.log(props)
   // props.projects is an array. its alllll of our projects
   // let project = props.projects[props.match.params.id - 1] // bc index starts at 0
-  console.log(project)
 
     let project = props.projects.filter( project => project.id == props.match.params.id)[0]
-
+  // console.log(project)
     return (
         // going to need access to a Project, so pass down props
         // CANNOT RENDER AN OBJ inside jsx tags
