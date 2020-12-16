38ca44455ca85d459bd52ee310d1a4feb0b55c65
no0ro <nrolwes@gmail.com>
2020-12-16 09:23:11 -0500

    add in a redirect so that if no show page, will send to index page instead of throwing error

--- issue-tracker/src/components/Project.js
+++ issue-tracker/src/components/Project.js
@@ -14,6 +14,7 @@ const Project = (props) => {
         // going to need access to a Project, so pass down props
         // CANNOT RENDER AN OBJ inside jsx tags
       <li>
+        {project ? null : <Redirect to='/projects'/>}
         {project ? project.name : null}
       </li>
     )
