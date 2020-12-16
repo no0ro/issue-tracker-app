3436ae95ea40b97658d1419cf16448c5d9cbe2ef
no0ro <nrolwes@gmail.com>
2020-12-16 06:57:14 -0500

    changed component namde from ProjectShow to Project

---
+++ issue-tracker/src/components/Project.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+// functional bc just diaplaying. doesnt need to update or directly access state
+const Project = (props) => {
+
+    return (
+        // going to need access to a Project, so pass down props
+      <div>
+        hi
+      </div>
+    )
+
+}
+
+export default Project
