50b7f2ddc338da241dd1f1c983e2f20cdfd2c598
no0ro <nrolwes@gmail.com>
2020-12-16 09:47:10 -0500

    added ItemInput component with basic syntax

---
+++ issue-tracker/src/components/ItemInput.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class ItemInput extends React.Component {
+
+    render() {
+        return (
+            <div>
+                inside ItemInput.js
+            </div>
+        )
+    }
+}
+
+export default ItemInput
