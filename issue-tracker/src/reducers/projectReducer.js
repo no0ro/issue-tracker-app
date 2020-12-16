3eb652679c0d2d58a7526bcf362ca7443a632fbd
no0ro <nrolwes@gmail.com>
2020-12-16 11:29:12 -0500

    added ADD_ISSUE to reducer

--- issue-tracker/src/reducers/projectReducer.js
+++ issue-tracker/src/reducers/projectReducer.js
@@ -6,6 +6,15 @@ export default function projectReducer(state = {projects: []}, action) {
             return {projects: action.payload}
         case 'ADD_PROJECT':
             return {...state, projects: [...state.projects, action.payload]}
+        case 'ADD_ISSUE':
+            let projects = state.projects.map(project => {
+                if (project.id === action.payload.id){
+                    return action.payload
+                } else {
+                    return project
+                }
+            })
+            return {...state, projects: projects}
         default:
             return state
             // always want to return SOME version of our state
