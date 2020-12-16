18d7d75447506f02bd8853c4479c7fd9a4397a8c
no0ro <nrolwes@gmail.com>
2020-12-16 11:20:54 -0500

    added fetch post call for addIssue

--- issue-tracker/src/actions/addIssue.js
+++ issue-tracker/src/actions/addIssue.js
@@ -1,7 +1,15 @@
-export const addIssue = () => {
+export const addIssue = (issue, projectId) => {
 
     return (dispatch) => {
-        
+        fetch(`http://localhost:3000/api/v1/projects/${projectId}/issues`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(issue)
+        })
+        .then(res => res.json())
+        .then(project => dispatch({type: 'ADD_ISSUE', payload: project}))
     }
 
 }
