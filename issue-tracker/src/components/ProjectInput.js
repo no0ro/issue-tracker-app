2fdfb2361221eb90f5e551528865dd0c2165a00b
no0ro <nrolwes@gmail.com>
2020-12-16 03:14:20 -0500

    added onchange listener and basic handler syntax

--- issue-tracker/src/components/ProjectInput.js
+++ issue-tracker/src/components/ProjectInput.js
@@ -3,15 +3,26 @@
 import React from 'react'
 
 class ProjectInput extends React.Component{
-    // SET STATE - giving form state and value make it a controlled form. 
-    state = { name: ''} // could use constructor syntax instead
+    // SET STATE - giving form state and value make it a controlled form.
+    state = { name: ''} 
+        // could use constructor syntax instead
+        // LOCAL state. not using redux store. 
+
+    // define the fn that will handle the onChange listener
+    handleChange = (e) => {
+        // e is auto passed in -- debugger
+        this.setState({
+            name: e.target.value
+        })
+        
+    }
 
     render() {
         return (
             <div>   
                 <form>
                     <label> Project Name: </label>
-                    <input type='text' placeholder="Name" value={this.state.name}/> 
+                    <input type='text' placeholder="Name" value={this.state.name} onChange={this.handleChange}/> 
                 </form>
             </div>
         )
