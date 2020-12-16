5229b53d143fa9b49bac3754da6102cc65ec0d32
no0ro <nrolwes@gmail.com>
2020-12-16 07:02:51 -0500

    passed props down to project component

--- issue-tracker/src/components/Projects.js
+++ issue-tracker/src/components/Projects.js
@@ -5,8 +5,11 @@ import Project from './Project'
 const Projects = (props) => {
     // console.log(props) // an obj of all the props passed in
     return (
+        // pass state down to Project (got it from props.projects has access to it)
         <div>   
-            {props.projects.map(project => <li key={project.id}> <Project/> </li>)}
+            {props.projects.map(project => 
+                <li key={project.id}> <Project project={project}/> </li>
+                )}
         </div>
     )
 }
