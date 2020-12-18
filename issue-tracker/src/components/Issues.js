b8eaee4085ec73e8e960c1449b7db92b369fd0d2
no0ro <nrolwes@gmail.com>
2020-12-18 15:40:29 -0500

    added delete button, event listener and event handler

--- issue-tracker/src/components/Issues.js
+++ issue-tracker/src/components/Issues.js
@@ -3,14 +3,18 @@ import React from 'react'
 const Issues = (props) => {
     console.log(props.issues)
 
+    handleDelete = (issue) => {
+        this.props.deleteIssue(issue.id, issue.project_id)
+    }
+
     return (
         <div>
             {props.issues && props.issues.map(issue => 
-                <li key={issue.id}>{issue.description}</li>
+                <li key={issue.id}>{issue.description} <button onClick={handleDelete}> Issue Resolved </button></li>
             )}
         </div>
     )
 }
 
 export default Issues
-{/* <li key={issue.id}>{issue.description}  {issue.done}  {issue.kind} {issue.working}</li> */}
+//{/* <li key={issue.id}>{issue.description}  {issue.done}  {issue.kind} {issue.working}</li> */}
