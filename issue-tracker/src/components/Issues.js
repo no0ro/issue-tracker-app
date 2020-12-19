a276fd5c5f91cf4429a05b7e5c7e6d2a9a22330d
no0ro <nrolwes@gmail.com>
2020-12-19 16:38:36 -0500

    imported connect and deleteIssue file,edited the listener call and handler method

--- issue-tracker/src/components/Issues.js
+++ issue-tracker/src/components/Issues.js
@@ -1,21 +1,21 @@
 import React from 'react'
-
+import {connect} from 'react-redux'
+import {deleteIssue} from '../actions/deleteIssue'
 
 const Issues = (props) => {
     // console.log(props.issues)
 
-const handleDelete = (issue) => {
-        this.props.deleteIssue(issue.id, issue.project_id)
-    }
+    const handleDelete = (issue) => {
+            props.deleteIssue(issue.id, issue.project_id);
+    };
 
     return (
         <div>
-            {props.issues && props.issues.map(issue => 
-                <li key={issue.id}>{issue.description} <button onClick={handleDelete}> Issue Resolved </button></li>
+            {props.issues && props.issues.map(issue => <li key={issue.id}>{issue.description} <button onClick={() => handleDelete(issue)}> Issue Resolved </button></li>
             )}
         </div>
-    )
+    );
 }
 
-export default Issues
+export default connect(null, {deleteIssue})(Issues)
 //{/* <li key={issue.id}>{issue.description}  {issue.done}  {issue.kind} {issue.working}</li> */}
