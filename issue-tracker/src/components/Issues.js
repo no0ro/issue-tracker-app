f84712df5e6d3027c03e730cebbd10029c73fd9a
no0ro <nrolwes@gmail.com>
2020-12-19 16:23:15 -0500

    commented out console.log

--- issue-tracker/src/components/Issues.js
+++ issue-tracker/src/components/Issues.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 
 const Issues = (props) => {
-    console.log(props.issues)
+    // console.log(props.issues)
 
 const handleDelete = (issue) => {
         this.props.deleteIssue(issue.id, issue.project_id)
