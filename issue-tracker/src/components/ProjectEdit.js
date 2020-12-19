ca950d27b21348602ddbcd08a6cc0664ebf1616a
no0ro <nrolwes@gmail.com>
2020-12-19 16:57:44 -0500

    added Prokjectg edit file and build out basic class syntax

---
+++ issue-tracker/src/components/ProjectEdit.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class ProjectEdit extends React.Component {
+
+    render() {
+        return(
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default ProjectEdit
