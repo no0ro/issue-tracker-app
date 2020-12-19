dcf870dd7740b1acd9adf22bd37b195e1153a8d6
no0ro <nrolwes@gmail.com>
2020-12-19 17:00:12 -0500

    added form

--- issue-tracker/src/components/ProjectEdit.js
+++ issue-tracker/src/components/ProjectEdit.js
@@ -5,6 +5,11 @@ class ProjectEdit extends React.Component {
     render() {
         return(
             <div>
+                <form onSubmit={this.handleSubmit}>
+                    <label> Project Name: </label>
+                    <input type='text' placeholder="Name" value={this.state.name} name="name" onChange={this.handleChange}/> 
+                    <input type="submit"/>
+                </form>
 
             </div>
         )
