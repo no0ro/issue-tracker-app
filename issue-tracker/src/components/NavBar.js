acbb0379a4ae968f983cbd7be0321cc9d97e4b1a
no0ro <nrolwes@gmail.com>
2020-12-19 22:11:15 -0500

    imported bootstrap navbar and nav, added bootstrap navbar styling

--- issue-tracker/src/components/NavBar.js
+++ issue-tracker/src/components/NavBar.js
@@ -1,12 +1,20 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import Navbar from 'react-bootstrap/NavBar'
+import Nav from 'react-bootstrap/Nav'
 
 const NavBar = (props) => {
     return (
         <div>
-            <Link to='/projects' style={{paddingRight: '10px'}}> All Projects</Link>
-            <Link to='/projects/new'style={{paddingRight: '10px'}}> Add a Project</Link>
-            <Link to='/'>Homepage</Link>
+            <Navbar bg="light" expand="lg">
+            <Navbar.Brand href="#home">Issue Tracker</Navbar.Brand>
+                {/* <Nav className="mr-auto"> */}
+                    <Link to='/'>Homepage</Link>
+                    <Link to='/projects' style={{paddingRight: '10px'}}> All Projects</Link>
+                    <Link to='/projects/new'style={{paddingRight: '10px'}}> Add a Project</Link>
+                    
+            {/* </Nav> */}
+            </Navbar>
         </div>
     )
 }
