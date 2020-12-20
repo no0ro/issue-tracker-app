1923bfb98aab0c8c150ce043f2bbadd15f16bc63
no0ro <nrolwes@gmail.com>
2020-12-19 21:28:08 -0500

    added test idea for debugging

--- issue-tracker/src/actions/deleteIssue.js
+++ issue-tracker/src/actions/deleteIssue.js
@@ -7,3 +7,15 @@ export const deleteIssue = (issueId, projectId) => {
         .then(project => dispatch({type: 'DELETE_ISSUE', payload: project}))
     }
 }
+
+
+// try turning anonymous fn into named fn?
+// export function deleteIssue(issueId, projectId) {
+//     return (dispatch) => {
+//        return fetch(`http://localhost:3000/api/v1/projects/${projectId}/issues/${issueId}`, {
+//             method: 'DELETE'
+//         })
+//         .then(res => res.json())
+//         .then(project => dispatch({type: 'DELETE_ISSUE', payload: project}))
+//     }
+// }
