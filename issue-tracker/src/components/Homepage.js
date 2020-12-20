be0c7a7adb8f184e8d8c87cfc26ea521b1d64fe1
no0ro <nrolwes@gmail.com>
2020-12-19 22:26:11 -0500

    imported bootstrap jumbotron and button, added basic syntax

--- issue-tracker/src/components/Homepage.js
+++ issue-tracker/src/components/Homepage.js
@@ -1,10 +1,19 @@
 import React from 'react'
+import Jumbotron from 'react-bootstrap/Jumbotron'
+import Button from 'react-bootstrap/Button'
 
 const Homepage = () => {
     return(
-        <div>
-            inside Homepage.js
-        </div>
+        <Jumbotron>
+            <h1>Welcome to Issue Tracker!</h1>
+            <p>
+                "Details about project will go here "
+            </p>
+            <p>
+                <Button variant="dark">View All Projects</Button>
+            </p>
+        </Jumbotron>
+    
     )
 }
 
