b2b2489a04c40071ec4f8904d60acb83cfea5764
no0ro <nrolwes@gmail.com>
2020-12-21 12:17:30 -0500

    added stateless component to display issues that are currently being worked on

---
+++ issue-tracker/src/components/IssuesWorking.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const IssuesWorking = (props) => {
+
+    return (
+        <div>
+            {/* <onChange={this.onChange}> */}
+            Inside IssuesWOrking.js
+        </div>
+
+    )
+}
+
+export default IssuesWorking
+
+// pass issues components into this  to render
