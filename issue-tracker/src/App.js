7e43622010ef47a8e0c85b5fea9ed189d481a5a1
no0ro <nrolwes@gmail.com>
2020-12-21 10:53:20 -0500

    added bootstrap Container component

--- issue-tracker/src/App.js
+++ issue-tracker/src/App.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import ProjectsContainer from './containers/ProjectsContainer' 
+import Container from 'react-bootstrap/Container'
 
 class App extends React.Component {
   // 2. renders Projects Container 
   render() {
     return (
-      <div style={{padding: '15px'}} className="App">
-        <ProjectsContainer/>
-      </div>
+      <Container fluid="md">
+        {/* <div style={{padding: '15px'}} className="App"> */}
+        <div className="App">
+
+          <ProjectsContainer/>
+        </div>
+      </Container>
     );
   }
 }
