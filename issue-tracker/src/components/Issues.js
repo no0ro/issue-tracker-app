c7a6e2170238729280afa6d64c70cedef22060fa
no0ro <nrolwes@gmail.com>
2020-12-21 10:51:57 -0500

    added listener  individual issue and did an alert test

--- issue-tracker/src/components/Issues.js
+++ issue-tracker/src/components/Issues.js
@@ -5,13 +5,29 @@ import {deleteIssue} from '../actions/deleteIssue'
 const Issues = (props) => {
     // console.log(props.issues)
 
+        // constructor(props){
+    //     super(props)
+    //     this.state = {
+
+    //     }
+    // }
+    
+
+    // create fn to handle event
+    const onChange = () => {
+        alert("onChange Test")
+        // this.setState({ color: 'green'})
+        //  <div onClick={this.props.onChange}>
+    }
+
+
     const handleDelete = (issue) => {
         // debugger
         props.deleteIssue(issue.id, issue.project_id);
     };
 
     return (
-        <div>
+        <div onClick={() => onChange()}>
             {props.issues && props.issues.map(issue => <li key={issue.id}>{issue.description} <button onClick={() => handleDelete(issue)}> Issue Resolved </button></li>
             )}
         </div>
@@ -20,3 +36,6 @@ const Issues = (props) => {
 
 export default connect(null, {deleteIssue})(Issues)
 //{/* <li key={issue.id}>{issue.description}  {issue.done}  {issue.kind} {issue.working}</li> */}
+
+// if working === true, pass the component to IssuesWorking to Render there 
+// if done === true, pass component to done component
