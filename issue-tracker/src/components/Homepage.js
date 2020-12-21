4a34109a8c665fb3d98531a89311bb942e68ff62
no0ro <nrolwes@gmail.com>
2020-12-21 10:50:34 -0500

    added welcome text

--- issue-tracker/src/components/Homepage.js
+++ issue-tracker/src/components/Homepage.js
@@ -7,7 +7,7 @@ const Homepage = () => {
         <Jumbotron>
             <h1>Welcome to Issue Tracker!</h1>
             <p>
-                "Details about project will go here "
+                A space for users to keep track of project issues. Simply create an project and add issues as they arise. Update the status of issues from active, to working, and finally to resolved. 
             </p>
             <p>
                 <Button variant="dark">View All Projects</Button>
