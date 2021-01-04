68f20a84df2bad226ec62503b611ae402a32e5d3
no0ro <nrolwes@gmail.com>
2021-01-04 08:46:12 -0500

    added code to display issues in the working category

--- issue-tracker/src/components/IssuesWorking.js
+++ issue-tracker/src/components/IssuesWorking.js
@@ -1,16 +1,45 @@
 import React from 'react'
 
+
 const IssuesWorking = (props) => {
 
-    return (
-        <div>
-            {/* <onChange={this.onChange}> */}
-            <h3>Working: </h3>
-        </div>
+    const onChange = () => {
+        alert("clicked")
+        // let changeColor = props.working === true ? ""
+    }
 
-    )
+    return ( 
+
+    <div>
+            <h3>Working: </h3>
+        {props.issues && props.issues.map( (issue) => 
+                <li key={issue.id} onClick={() => onChange()}>
+                        {issue.description} -  {issue.kind} - {issue.working}  
+                        {/* <Button variant="dark" size="sm" onClick={() => handleDelete(issue)}> x </Button> */}
+                    </li>
+                    
+                )}
+        
+            </div>
+        );
 }
 
 export default IssuesWorking
 
 // pass issues components into this  to render
+
+
+
+// return ( 
+//     <div onClick={() => onChange()}>
+//                 {props.issues && props.issues.map( (issue) => 
+//                     <li key={issue.id}>
+//                         {issue.description} -  {issue.kind} - {issue.working}  
+//                         <Button variant="dark" size="sm" onClick={() => handleDelete(issue)}> x </Button>
+//                     </li>
+                    
+//                 )}
+//                 <IssuesWorking issues={props.issues}/>
+        
+//             </div>
+//         );
